5bca6387b8227cb6154207ea824d38d0cdfe68be
liushufan1104 <aeeoprsd110414@gmail.com>
2019-12-26 19:08:14 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.lib;
+import java.util.Scanner;
+public class MyClass {
+    public static void main ( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿é¤J¦r¦ê:");
+        String string = scanner.next();
+        for(int i=string.length();i>0;i--){
+            System.out.print(string.charAt(i-1));
+        }
+    }
+}
